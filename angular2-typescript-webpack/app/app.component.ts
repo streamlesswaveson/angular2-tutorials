381ca455f1043ee9d6d6ca33eff7f21aad44587a
Chad Blair <chadblair@gmail.com>
2016-12-08 15:39:56 -0800

    another tutorial

---
+++ angular2-typescript-webpack/app/app.component.ts
@@ -0,0 +1,10 @@
+/// <reference path="../typings/index.d.ts" />
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>My First Angular 2 App</h1>`
+})
+
+export class AppComponent {}
