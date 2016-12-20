b88c8b1a5cc329d806e7b6675787770e8e730e02
Streamless Waveson <streamlesswaveson@gmail.com>
2016-12-20 11:26:15 -0800

    *ngFor demo

--- 02-directives/src/app/app.component.ts
+++ 02-directives/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   private switch:boolean = false;
-
+  private items = [1,2,3,4,5,6];
   onSwitch() {
     this.switch = !this.switch;
   }
