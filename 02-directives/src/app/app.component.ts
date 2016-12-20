1ba1920b64c0b5e8ba153f64f020bc5c9b2b428a
Streamless Waveson <streamlesswaveson@gmail.com>
2016-12-20 11:21:56 -0800

    *ngIf demo

--- 02-directives/src/app/app.component.ts
+++ 02-directives/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dir works!';
+
+  private switch:boolean = false;
+
+  onSwitch() {
+    this.switch = !this.switch;
+  }
+
 }
